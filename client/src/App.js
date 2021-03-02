c9d97f11e034c81add80b0ef8f49359a3c3840d8
fulinliu <fulin426@gmail.com>
2021-03-01 16:06:03 -0800

    new appointment form front end and post request back end

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
-import { Container, Box } from '@material-ui/core';
+import { Container, Box, TextField, Button } from '@material-ui/core';
 import { DataGrid } from '@material-ui/data-grid';
 import Physicians from './Components/Physicians';
 import './App.css';
@@ -19,6 +19,10 @@ function App() {
   const [selectedNameId, setSelectedNameId] = useState('1');
   const [displayName, setDisplayName] = useState('');
   const [displayEmail, setDisplayEmail] = useState('');
+  //form states
+  const [name, setName] = useState('');
+  const [time, setTime] = useState('');
+  const [kind, setKind] = useState('');
 
   useEffect(()=> {
     const getNames = async () => {
@@ -44,8 +48,6 @@ function App() {
 
     for (let appointment of appointments) {
       if (appointment.id === selectedNameId) {
-        console.log(appointment.id)
-        console.log(appointment.schedule);
         setCurrentSchedule(appointment.schedule);
       }
     }
@@ -54,6 +56,21 @@ function App() {
   const clickName = (e) => {
     const id = e.currentTarget.dataset.id;
     setSelectedNameId(id);
+  }
+
+  const submitAppointment = (e) => {
+    e.preventDefault();
+    const newAppointment = {id: currentSchedule.length + 1, kind, name,time};
+    setCurrentSchedule([...currentSchedule, newAppointment])
+    
+    axios.post('/api/add', {
+      id: currentSchedule.length + 1, 
+      kind, 
+      name,
+      time,
+      selectedNameId,
+    })
+    .then(res => console.log(res));
   }
 
   return (
@@ -68,16 +85,27 @@ function App() {
         <Box ml={4}>
           <h1>{displayName}</h1>
           <h4>{displayEmail}</h4>
-          <div style={{ height: 400, width: 550 }}>
+          <div style={{ height: 500, width: 550 }}>
             <DataGrid 
               rows={currentSchedule} 
               columns={columns} 
-              pageSize={5} 
+              pageSize={10} 
               hideFooterPagination={true} 
             />
           </div>
         </Box>
       </Box>
+      <Box mt={4}>
+        <form noValidate autoComplete="off" onSubmit={(e) => submitAppointment(e)}>
+          <TextField onChange={(e) => setName(e.target.value)} id="standard-basic" label="Name" variant="outlined"/>
+          <TextField onChange={(e) => setTime(e.target.value)} id="filled-basic" label="Time" variant="outlined" />
+          <TextField onChange={(e) => setKind(e.target.value)} id="outlined-basic" label="Kind" variant="outlined" />
+          <div>
+            <Button type='submit'>Submit</Button>
+          </div>
+        </form>
+      </Box>
+      
     </Container>
   );
 }
